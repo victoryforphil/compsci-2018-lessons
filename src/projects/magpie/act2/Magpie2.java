0313d3a05ba60ac3f6c3bb013a2c65323b6812d5
victoryforphil <victoryforphil@gmail.com>
2018-09-23 17:19:16 -0700

    Mag Pie Done

---
+++ src/projects/magpie/act2/Magpie2.java
@@ -0,0 +1,125 @@
+/**
+ * A program to carry on conversations with a human user.
+ * This is the initial version that:  
+ * <ul><li>
+ *       Uses indexOf to find strings
+ * </li><li>
+ * 		    Handles responding to simple words and phrases 
+ * </li></ul>
+ * This version uses a nested if to handle default responses.
+ * @author Laurie White
+ * @version April 2012
+ */
+public class Magpie2
+{
+	/**
+	 * Get a default greeting 	
+	 * @return a greeting
+	 */
+	public String getGreeting()
+	{
+		return "Hello, let's talk.";
+	}
+	
+	/**
+	 * Gives a response to a user statement
+	 * 
+	 * @param statement
+	 *            the user statement
+	 * @return a response based on the rules given
+	 */
+	public String getResponse(String statement)
+	{
+		String response = "";
+
+		if(response.trim().length() <= 0){
+			return "Say something please.";
+		}
+
+		if (statement.indexOf("no") >= 0)
+		{
+			response = "Why so negative?";
+		}
+		else if (statement.indexOf("mother") >= 0
+				|| statement.indexOf("father") >= 0
+				|| statement.indexOf("sister") >= 0
+				|| statement.indexOf("brother") >= 0)
+		{
+			response = "Tell me more about your family.";
+		}
+		else if (statement.indexOf("dog") >= 0
+				|| statement.indexOf("cat") >= 0)
+		{
+			response = "Tell me more about your pets.";
+		}
+		else if (statement.indexOf("swing") >= 0)
+		{
+			response = "She sounds greeeaaaatttttt!";
+		}
+		else if (statement.indexOf("robotics") >= 0)
+		{
+			response = "WHO ARE WE? 4RP!!!";
+		}
+		else if (statement.indexOf("gths") >= 0)
+		{
+			response = "why is asb so mean :(";
+		}
+		else if (statement.indexOf("java") >= 0)
+		{
+			response = "Why u take my ram!";
+		}
+		else
+		{
+			response = getRandomResponse();
+		}
+		return response;
+	}
+
+	/**
+	 * Pick a default response to use if nothing else fits.
+	 * @return a non-committal string
+	 */
+	private String getRandomResponse()
+	{
+		final int NUMBER_OF_RESPONSES = 7;
+		double r = Math.random();
+		int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
+		String response = "";
+		
+		if (whichResponse == 0)
+		{
+			response = "Interesting, tell me more.";
+		}
+		else if (whichResponse == 1)
+		{
+			response = "Hmmm.";
+		}
+		else if (whichResponse == 2)
+		{
+			response = "Do you really think so?";
+		}
+		else if (whichResponse == 3)
+		{
+			response = "You don't say.";
+		}
+		else if (whichResponse == 4)
+		{
+			response = "Much wow. Such intrest.";
+		}
+		else if (whichResponse == 5)
+		{
+			response = "K.";
+		}
+		else if (whichResponse == 6)
+		{
+			response = "Thank you Kayne. Very cool!";
+		}
+
+
+		return response;
+	}
+}
+//QUESTIONS
+/*
+1) It will reply to the first word it locates in the sentence, example Know will reply with the No response. This is due to the fact indexOf does not take into account spaces.
+ */
